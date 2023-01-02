c53158514cb92569e370adc257f5f1c5bf026a97
Luis <luiszelarrayan@hotmail.com>
2023-01-02 14:44:20 -0300

    Exception

---
+++ src/com/exception/NameFormatException.java
@@ -0,0 +1,11 @@
+package com.exception;
+
+public class NameFormatException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public NameFormatException(String errorMessage){
+        super(errorMessage);
+    }
+
+}
---
+++ src/com/exception/ThorwMain.java
@@ -0,0 +1,41 @@
+package com.exception;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class ThorwMain {
+
+    public static void main(String[] args) {
+
+        try{
+            leerNombres();
+        }catch (NameFormatException e){
+            e.printStackTrace();
+        }
+        System.out.println("cerrando teclado");
+    }
+
+    /**
+     * Metodo que lee nombres de consola y verifica que tengan la longitud igual o mayor que 8
+     * caracteres
+     * @throws NameFormatException
+     */
+
+    private static void leerNombres() throws NameFormatException{
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un nombre");
+        while(teclado.hasNext()){
+            System.out.println("Introduce un nombre");
+            String nombre = teclado.next();
+            if(nombre.length()< 8){
+                teclado.close();
+                throw new NameFormatException("El nombre debe contener minimo 8 caracteres");
+            }
+        }
+
+        teclado.close();
+    }
+
+
+}
